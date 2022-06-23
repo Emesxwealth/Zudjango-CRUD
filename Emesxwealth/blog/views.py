bd76fb9db861f18f8207d40ee6a1e812d4efc28a
Nworie Chukwuemeka <cemexfarm@gmail.com>
2022-06-23 12:43:27 +0100

    zuri crud project first git push

--- Emesxwealth/blog/views.py
+++ Emesxwealth/blog/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 
 from django.views.generic.edit import PostListView
-from .models import Blogapp, Post
+from .models import BlogApp, Post
+
 
 class BlogAppPostListView(PostListView):
     model = Post
-    fields = "__all__"
+    fields = ["__all__"]
     success_url  = reverse_lazy("blog:all")
 
 class BlogAppPostDetailView(PostDetailView):
@@ -14,12 +15,12 @@ class BlogAppPostDetailView(PostDetailView):
 
 class BlogAppPostUpdateView(PostUpdateView): 
     model = Post
-    fields = "__all__"
+    fields = ["__all__"]
     success_url  = reverse_lazy("blog:all")   
 
 class BlogAppPostDeleteView(PostDeleteView):
     model = Post
-    fields = "__all__"
+    fields = ["__all__"]
     success_url  = reverse_lazy("blog:all")  
 
 
