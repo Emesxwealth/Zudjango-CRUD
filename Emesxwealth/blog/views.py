5c8e3e0c074221ea41deda3c3ca6c973cdba430d
Nworie Chukwuemeka <cemexfarm@gmail.com>
2022-06-23 07:32:58 +0100

    first push crud zuri project

---
+++ Emesxwealth/blog/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.urls import reverse_lazy
+
+from django.views.generic.edit import PostListView
+from .models import Blogapp, Post
+
+class BlogAppPostListView(PostListView):
+    model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
+
+class BlogAppPostDetailView(PostDetailView):
+    model = Post
+
+class BlogAppPostUpdateView(PostUpdateView): 
+    model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")   
+
+class BlogAppPostDeleteView(PostDeleteView):
+    model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")  
+
+
